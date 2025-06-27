573f0a80f40e6063476968989ef6ec56502e7713
Rohitendra <rohitendra.singh75@gmail.com>
2025-06-27 21:18:01 +0530

    Final changes without mongo injection and other security concerns

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const MongoStore = require('connect-mongo');
+
+
+
+
+// const dbURL = process.env.DB_URL;
+const dbURL = 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbURL);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+
+
 
 
 const { log } = require('console');
@@ -38,8 +55,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const store = MongoStore.create({
+  mongoUrl: dbURL,
+  secret: 'itsnotaspecialsecret',
+  touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e){
+  console.log("Session Store Error");;  
+})
 
 const sessionConfig = {
+  store ,
   secret : 'itsnotaspecialsecret',
   resave: false,
   saveUninitialized: true,
@@ -48,7 +75,6 @@ const sessionConfig = {
     expires : Date.now() + 1000*60*60*24*7,
     maxAge: 1000*60*60*24*7
   }
-  // store:
 }
 app.use(session(sessionConfig));
 app.use(flash());
@@ -61,6 +87,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) =>{
+  console.log(req.query);  
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
@@ -98,15 +125,6 @@ const validateReview = (req,res,next) =>{
 
 
 
-
-
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database Connected");
-})
 
 app.get('/', (req, res) => {
   res.render('home');
